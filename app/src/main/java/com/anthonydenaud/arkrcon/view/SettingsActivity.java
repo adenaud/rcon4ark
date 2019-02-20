e03b5aecf2147f128c1fdf66a76d49c6dbb2c10a
Anthony Denaud <anthonydenaud@gmail.com>
2019-02-20 18:21:52 +0000

    New themes

--- app/src/main/java/com/anthonydenaud/arkrcon/view/SettingsActivity.java
+++ app/src/main/java/com/anthonydenaud/arkrcon/view/SettingsActivity.java
@@ -1,14 +1,17 @@
 package com.anthonydenaud.arkrcon.view;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.MenuItem;
 
+import com.anthonydenaud.arkrcon.R;
 import com.anthonydenaud.arkrcon.fragment.SettingsFragment;
 
 import roboguice.activity.RoboActionBarActivity;
 
 
-public class SettingsActivity extends RoboActionBarActivity {
+public class SettingsActivity extends ThemeActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +26,18 @@ public class SettingsActivity extends RoboActionBarActivity {
             finish();
             return true;
         }
-
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void applyTheme(String theme) {
+        if(THEME_DARK.equals(theme)){
+            setTheme(R.style.AppDarkTheme);
+            getApplication().setTheme(R.style.AppDarkTheme);
+        }
+        if(THEME_ARK.equals(theme)){
+            setTheme(R.style.AppArkTheme);
+            getApplication().setTheme(R.style.AppArkTheme);
+        }
     }
 }
---
+++ app/src/main/java/com/anthonydenaud/arkrcon/view/ThemeActivity.java
@@ -0,0 +1,58 @@
+package com.anthonydenaud.arkrcon.view;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+
+import com.anthonydenaud.arkrcon.R;
+
+import roboguice.activity.RoboActionBarActivity;
+
+/**
+ * Created by Anthony on 15/07/2017.
+ */
+
+public class ThemeActivity extends RoboActionBarActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+
+    protected static final String THEME_LIGHT = "light";
+    protected static final String THEME_DARK = "dark";
+    protected static final String THEME_ARK = "ark";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        preferences.registerOnSharedPreferenceChangeListener(this);
+        setTheme(false);
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+    private void setTheme(boolean recreate){
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String theme = preferences.getString("theme",null);
+        applyTheme(theme);
+        if(recreate){
+            recreate();
+        }
+    }
+
+    protected void applyTheme(String theme){
+        if(THEME_DARK.equals(theme)){
+            getApplication().setTheme(R.style.AppDarkTheme);
+            setTheme(R.style.AppDarkTheme_NoActionBar);
+        }
+        if(THEME_ARK.equals(theme)){
+            getApplication().setTheme(R.style.AppArkTheme);
+            setTheme(R.style.AppArkTheme_NoActionBar);
+        }
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+        if("theme".equals(s)){
+            setTheme(true);
+        }
+    }
+}
